13ddf3f4bfeab2521963a138eb81f43b7b220e97
v0vanch <vova.menkin@mail.ru>
2018-03-01 14:11:07 +0500

    commit

--- client.py
+++ client.py
@@ -1,15 +1,3 @@
-#import socket
-
-#sock = socket.socket()
-#sock.connect(('ekb-04-479', 9090))
-
-#while True:
-    #mes = input('enter message: ')
-    #if mes == 'stop':
-        #break
-    #sock.send(mes.encode("utf-8"))
-#sock.close()
-
 import socket
 from tkinter import *
 
