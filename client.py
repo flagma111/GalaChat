e72535bdf910dcc9dd445b0641990054d488edf9
v0vanch <vova.menkin@mail.ru>
2018-03-06 11:35:15 +0500

    Доработка клиента

--- client.py
+++ client.py
@@ -1,23 +1,15 @@
+# -*- coding: utf-8 -*-
 import socket, time
 import tkinter as tk
-
-def loopproc(): 
-    log.see(END)
-    sock.settimeout(0.01)
-    try:
-        message = sock.recv(128)
-        log.insert(END,message.decode("utf-8")+"\n")
-    except:
-        tk.after(1,loopproc)
-        return
-    tk.after(1,loopproc)
-    return
+import json
 
 class Client:
     
     def __init__(self):
         self.user = User()
         self.server = ("localhost", 9090)
+        self.connection_try = 0
+        self.connection_timeout = 5
 
         self.init_GUI()
         self.init_network()
@@ -31,39 +23,59 @@ class Client:
         self.root.protocol("WM_DELETE_WINDOW", self.onclose)
         
         self.user_nick = tk.StringVar(self.root, self.user.nick)
+        self.status = tk.StringVar(self.root, "Running...")
 
         self.e_nick = tk.Label(self.root, textvariable=self.user_nick)
         self.e_log = tk.Text(self.root)
         self.e_input = tk.Text(self.root, height=3)
+        self.e_status = tk.Label(self.root, textvariable=self.status)
 
+        self.e_status.pack(side="bottom", fill="x", expand="true")
         self.e_input.pack(side="bottom", fill="x", expand="true")
         self.e_nick.pack(side="bottom", fill="x", expand="true")
         self.e_log.pack(side="top", fill="both", expand="true")
         
-
-        self.e_input.bind("<Return>", self.send_message)
+        self.e_input.bind("<Shift-Return>", self.send_message)
         self.e_input.focus_set()
 
     def init_network(self):
         self.network = Network(self.server[0], self.server[1])
-        self.root.after(0, self.connect)
+        self.root.after(10, self.connect)
 
     def end_init(self):
         self.root.mainloop()
 
     def connect(self):
+        self.connection_try = self.connection_try + 1
         try:
             self.network.connect()
-            self.e_log.insert(tk.END,"Connected to" + self.network.addr + ":" + self.network.port + "\n")
-            self.root.after(0, revc_message)
-        except ConnectionRefusedError:
-            self.e_log.insert(tk.END,"Could not connect to server, retrying in 5 seconds...\n")
-            self.root.after(5000, self.connect)
+            self.status.set("Подключен к " + str(self.network.addr) + ":" + str(self.network.port))
+            self.root.after(1, self.recv_message)
+            self.connection_try = 0
+        except socket.timeout:
+            self.status.set("Сервер не отвечает, попытка подключения через " + str(self.connection_timeout) + " секунд... (Попытка " + str(self.connection_try) + ")")
+        finally:
+            self.root.after(self.connection_timeout * 1000, self.connect)
 
-    def send_message(self):
-        message = self.e_input.get(1.0, END)
-        self.e_input.set("")
-        self.network.send()
+    def clear_input(self):
+        self.e_input.delete(1.0, tk.END)
+
+    def send_message(self, event):
+        message = self.e_input.get(1.0, tk.END).strip()
+        self.root.after(1, self.clear_input)
+        self.network.send(message.encode("utf-8"))
+
+    def recv_message(self):
+        try:
+            message = self.network.recv()
+            obj = json.loads(message)
+            #if (obj["type"] == "message"):
+            self.e_log.insert(tk.END, message.decode("utf-8")+"\n")
+            self.e_log.see(tk.END)
+            #elif (obj["type"] == "auth_reg"):
+                
+        finally:
+            self.root.after(10, self.recv_message)
 
     def onclose(self):
         self.network.destroy()
@@ -77,23 +89,29 @@ class Network:
 
         self.socket = socket.socket()
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 2)
+        self.socket.settimeout(0.01)
 
     def connect(self):
         self.socket.connect((self.addr, self.port))
     
+    def send(self, message):
+        self.socket.send(message)
+
+    def recv(self):
+        message = self.socket.recv(4096)
+        return message
+
     def destroy(self):
         self.socket.close()
 
 class User:
 
-    def __init__(self, nick = ""):
-        if (nick == ""):
-            ts = int(time.time() * 1000)
-            offset = int(ts / 1000000) * 1000000
-            nick = "Guest" + str(ts - offset)
-        self.nick = nick
-    
-    def setData(self, nick):
+    def __init__(self, uid = 0, nick = ""):
+        #if (nick == ""):
+        #    ts = int(time.time() * 1000)
+        #    offest = int(ts / 1000000) * 1000000
+        #    nick = "Guest" + str(ts - offset)
+        self.id = uid
         self.nick = nick
 
 client = Client()
